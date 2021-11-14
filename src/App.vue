1a861295ec5828cb9bf5c2268064506454b52f32
dava667 <daviddavtiah1@gmail.com>
2021-11-14 23:19:30 +0600

    day-3
    day-3

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<transition name="modal">
+<div class="wrapper">
+<div class="wrapper-content">
+<section>
+
+<div class="container">
+
+<button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
+
+  <modals title="First modal" v-show="modalFirst" @close="modalFirst = !modalFirst">
+  <div slot="body">
+<p>See how my work works</p>
+<button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well done</button>
+</div>
+  </modals>
+</div>
+</section>
+
+</div>
+</div>
+</transition>
+</template>
+
+<script>
+import modals from'@/components/Modal.vue'
+export default{
+components:{
+  modals
+},
+data(){
+  return{
+   modalFirst:false
+  }
+}
+}
+</script>
